447d16c9ec630f5ffdd1af39d84c482d625ecf61
SirHenrique <henriquenicolli123@gmail.com>
2024-05-28 18:56:53 -0300

    Criação metodo pagamento anual e mensal

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -2,6 +2,7 @@
 	import { NavigationContainer } from "@react-navigation/native";
 	import { createStackNavigator } from "@react-navigation/stack";
 import Camera from "screens/camera";
+import Pagamento from "screens/pagamento";
 
 	import Profile from "screens/profile";
 
@@ -9,7 +10,8 @@ import Camera from "screens/camera";
 		Overview: undefined;
 		Details: { name: string };
 		Profile: { name: string};
-		Camera: {name: string}
+		Camera: {name: string};
+		Pagamento: {name: string};
 	};
 
 	const Stack = createStackNavigator<RootStackParamList>();
@@ -18,9 +20,10 @@ import Camera from "screens/camera";
 
 		return (
 			<NavigationContainer>
-				<Stack.Navigator initialRouteName="Camera" screenOptions={{ headerShown: false }}>
+				<Stack.Navigator initialRouteName="Pagamento" screenOptions={{ headerShown: false }}>
 					<Stack.Screen name="Profile" component={Profile}  />
 					<Stack.Screen name="Camera" component={Camera} />
+					<Stack.Screen name="Pagamento" component={Pagamento} />
 				</Stack.Navigator>
 			</NavigationContainer>
 		);
